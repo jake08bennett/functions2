9ceb1b557e62e2282e28db8eada91e99fadc9086
Jacob John Bennett <bennettjj@dcsdk12.org>
2016-08-31 12:20:47 -0600

    finished ice cream and squirrel party

--- functions.py
+++ functions.py
@@ -18,13 +18,9 @@
 # TODO - write elevenish
 
 def elevenish(n):
-	if n % 11 == 0:
-		return True
-	elif n % 11 == 1:
-		return True
-	else:
-		return False
-		
+	return n % 11 == 0 or n % 11 == 1
+
+
 
 
 
@@ -33,14 +29,56 @@ print elevenish(11) #True
 print elevenish(12) #True
 print elevenish(13) #False
 print elevenish(22) #True
-print elevenish(23) #False
+print elevenish(23) #True
 print elevenish(5) #False
 
 
-	
+print "\n"
 # TODO - write ice_cream_party
 
+def ice_cream_party(ice_cream, candy):
+	if candy < 5 or ice_cream < 5:
+		return 0
+	elif (candy / ice_cream) == 2 or (ice_cream / candy) == 2:
+		return 2
+	elif candy >= 5 and ice_cream >= 5:
+		return 1
+
+
+
+
+
+print ice_cream_party(10,5) #2
+print ice_cream_party(5,10) #2
+print ice_cream_party(5,5) #1
+print ice_cream_party(6,6) #1
+print ice_cream_party(6,3) #0
+print ice_cream_party(2,6) #0
+print ice_cream_party(2,3) #0
+
+
+
+print "\n"
 # TODO - write successful_squirrel_party
+
+def successful_squirrel_party(is_weekend, nuts):
+	if is_weekend:
+		if nuts > 40:
+			return True
+		else:
+			return False
+	else:
+		if nuts > 40 and nuts < 60:
+			return True
+		else:
+			return False
+
+
+print successful_squirrel_party(True,50) #True
+print successful_squirrel_party(True,70) #True
+print successful_squirrel_party(False,70) #False
+print successful_squirrel_party(False,20) #False
+print successful_squirrel_party(False,50) #True
 
 # TODO - write ticket
 
